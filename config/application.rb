17a53d1bc45d7f96e3e3efb4469e17a435a45e01
PeiDa Kuo <guo.peida@gmail.com>
2016-06-29 10:03:27 -0700

    modified to respond to X-origin requests with proper CORS header

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,12 @@ module StockDataApiWrapper
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :method => [:get, :post, :options]
+      end
+    end
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
